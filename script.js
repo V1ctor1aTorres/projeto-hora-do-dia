c01a64ca978b6983ac540b4ffd7a41b83a0a4932
victoria <148492863+V1ctor1aTorres@users.noreply.github.com>
2023-11-11 22:27:50 -0300

    projeto horas

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert('olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+   // var hora = data.getHours()
+    var hora =  20
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora <12) {
+        //bom dia 
+        img.src = 'imagens/manha1.png'
+        document.body.style.background = '#d5bba0'
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'imagens/tarde1.png'
+        document.body.style.background = '#bd933d'
+    } else {
+        //boa noite
+        img.src = 'imagens/noite1.png'
+        document.body.style.background = '#02254d'
+    }
+}
